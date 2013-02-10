1405a6127b7f5a18fe91b7da295721999364a367
patrick <patrick.c.jensen@gmail.com>
2013-02-10 13:05:36 -0500

    updated to html files to php extension

---
+++ content/work.php
@@ -0,0 +1,7 @@
+<a href=\"images/Resume_Jensen%2CPatrick.pdf\" target='new'>Resume</a> (PDF, 68 KB)<br><br>
+
+<a href=\"images/MapPortfolio.pdf\" target='new'>GIS Portfolio</a> (PDF, 1.5 MB)<br><br>
+
+<a href=\"images/GraphicsPortfolio_Jensen%2CPatrick_small.pdf\" target='new'>Graphics Portfolio</a> (PDF, 12.8 MB)<br><br><a href=\"images/PrincetonStudioPressCoverage.pdf\" target='new'>Studio Media Coverage</a> (PDF, 1.7 MB)<br><br>
+
+LBG Fellowship Media Coverage [<a href=\"http://news.rutgers.edu/medrel/special-content/new-fellowship-boost-20100504\" target='new'>1</a>, <a href=\"http://policy.rutgers.edu/news/fellows/berger-voorhees.php\" target='new'>2</a>]
