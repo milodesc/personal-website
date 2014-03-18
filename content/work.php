0bab747a98583ed56ac5a743a196d516d18f801f
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-03-17 22:51:27 -0400

    some content and css improvements for the work section

--- content/work.php
+++ content/work.php
@@ -1,7 +1,31 @@
-<a href=\"images/Resume_Jensen%2CPatrick.pdf\" target='new'>Resume</a> (PDF, 68 KB)<br><br>
+<div><a href='files/Resume_JensenPatrick.pdf' target='new'>Resume</a> (PDF, 68 KB)</div>
 
-<a href=\"images/MapPortfolio.pdf\" target='new'>GIS Portfolio</a> (PDF, 1.5 MB)<br><br>
+<div class='workPosition'>
+<div class='positionName'>Teaching Assistant: Comprehensive Planning Studio</div>
+<div class='positionTimeFrame'>September 2010 - December 2010</div>
+<div><span class='positionEmployer'>Rutgers University</span><span class='positionLocation'>New Brunswick, New Jersey</span></div>
+<div class='positionDescription'>Managed a team of 11 graduate students through a semester-long group project to recommend policies to Ocean City, NJ municipal officials that would spur hotel development in their municipality</div>
+</div>
 
-<a href=\"images/GraphicsPortfolio_Jensen%2CPatrick_small.pdf\" target='new'>Graphics Portfolio</a> (PDF, 12.8 MB)<br><br><a href=\"images/PrincetonStudioPressCoverage.pdf\" target='new'>Studio Media Coverage</a> (PDF, 1.7 MB)<br><br>
+<div class='workPosition'>
+    <div class='positionName'>Project Management Fellowship</div>
+    <div class='positionTimeFrame'>June 2010 - August 2010</div>
+    <div><span class='positionEmployer'>The Louis Berger Group</span><span class='positionLocation'>Mauritius, Africa</span></div>
+    <div class='positionDescription'>Managed an international team of architects, engineers and builders on a project to construct an expansion to Mauritius, Africa's international airport.</div>
+</div>
 
-LBG Fellowship Media Coverage [<a href=\"http://news.rutgers.edu/medrel/special-content/new-fellowship-boost-20100504\" target='new'>1</a>, <a href=\"http://policy.rutgers.edu/news/fellows/berger-voorhees.php\" target='new'>2</a>]
+
+<div class='workPosition'>
+    <div class='positionName'>Web Developer</div>
+    <div class='positionTimeFrame'>January 2005 - July 2009</div>
+    <div><span class='positionEmployer'>JPMorgan Chase</span><span class='positionLocation'>Columbus, OH</span></div>
+    <div class='positionDescription'>Developed new features for and fixed defects on a global, money market fund trading web application which handled more than $4 billion in trades per day.</div>
+</div>
+
+<!--
+<a href='images/MapPortfolio.pdf' target='new'>GIS Portfolio</a> (PDF, 1.5 MB)<br><br>
+
+<a href='images/GraphicsPortfolio_Jensen%2CPatrick_small.pdf' target='new'>Graphics Portfolio</a> (PDF, 12.8 MB)<br><br><a href='images/PrincetonStudioPressCoverage.pdf' target='new'>Studio Media Coverage</a> (PDF, 1.7 MB)<br><br>
+
+LBG Fellowship Media Coverage [<a href='http://news.rutgers.edu/medrel/special-content/new-fellowship-boost-20100504' target='new'>1</a>, <a href='http://policy.rutgers.edu/news/fellows/berger-voorhees.php' target='new'>2</a>]
+-->
