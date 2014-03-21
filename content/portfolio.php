6f8fcf835e53ded7fe20423b5cef9c1ed1dd5c32
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-03-21 12:57:35 -0400

    better css: from class to headers

--- content/portfolio.php
+++ content/portfolio.php
@@ -1,5 +1,4 @@
 <h3>Github</h3>
-Personal, open source projects including drupal modules, shell scripts and other fun.
+My personal, open source projects including drupal modules, shell scripts and other fun are available on my <a target="_blank" href="https://github.com/milodesc">Github page</a>.
 
 <h3>Professional Work</h3>
-lajkf
--- content/work.php
+++ content/work.php
@@ -2,29 +2,29 @@
 
 <div class='workPosition'>
     <h3>Web Manager</h3>
-    <div class='positionTimeFrame'>August 2011 - Current</div>
-    <div><span class='positionEmployer'>Transportation Alternatives</span><span class='positionLocation'>New York, New York</span></div>
+    <h4><span class='positionEmployer'>Transportation Alternatives</span><span class='positionLocation'>New York, New York</span></h4>
+    <h5>August 2011 - Current</h5>
     <div class='positionDescription'>Implement senior management's vision for the organization's online presence using Drupal and Convio Lumninate Online.</div>
 </div>
 
 <div class='workPosition'>
     <h3>Teaching Assistant: Comprehensive Planning Studio</h3>
-    <div class='positionTimeFrame'>September 2010 - December 2010</div>
-    <div><span class='positionEmployer'>Rutgers University</span><span class='positionLocation'>New Brunswick, New Jersey</span></div>
+    <h4><span class='positionEmployer'>Rutgers University</span><span class='positionLocation'>New Brunswick, New Jersey</span></h4>
+    <h5>September 2010 - December 2010</h5>
     <div class='positionDescription'>Managed a team of 11 graduate students through a semester-long group project to recommend policies to Ocean City, NJ municipal officials that would spur hotel development in their municipality</div>
 </div>
 
 <div class='workPosition'>
     <h3>Project Management Fellowship</h3>
-    <div class='positionTimeFrame'>June 2010 - August 2010</div>
-    <div><span class='positionEmployer'>The Louis Berger Group</span><span class='positionLocation'>Mauritius, Africa</span></div>
+    <h4><span class='positionEmployer'>The Louis Berger Group</span><span class='positionLocation'>Mauritius, Africa</span></h4>
+    <h5>June 2010 - August 2010</h5>
     <div class='positionDescription'>Managed an international team of architects, engineers and builders on a project to construct an expansion to Mauritius, Africa's international airport.</div>
 </div>
 
 
 <div class='workPosition'>
     <h3>Web Developer</h3>
-    <div class='positionTimeFrame'>January 2005 - July 2009</div>
-    <div><span class='positionEmployer'>JPMorgan Chase</span><span class='positionLocation'>Columbus, OH</span></div>
+    <h4><span class='positionEmployer'>JPMorgan Chase</span><span class='positionLocation'>Columbus, OH</span></h4>
+    <h5>January 2005 - July 2009</h5>
     <div class='positionDescription'>Developed new features for and fixed defects on a global, money market fund trading web application which handled more than $4 billion in trades per day.</div>
 </div>
