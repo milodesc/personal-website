d86a98846bcb0df020fa19f24d3ff6e8eca1ad64
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-03-24 08:38:06 -0400

    added portfolio content and made some styles more generics

--- content/portfolio.php
+++ content/portfolio.php
@@ -1,4 +1,37 @@
-<h3>Github</h3>
-My personal, open source projects including drupal modules, shell scripts and other fun are available on my <a target="_blank" href="https://github.com/milodesc">Github page</a>.
-
 <h3>Professional Work</h3>
+<h4><span class="horizItems"><a target="_blank" href="http://bikenyc.org">BikeNYC.org</a></span><span class="horizItems">2012 - Present</span></h4>
+<div class="minorText">
+    <p>This responsive Drupal website is the go-to resource for both crowdsourced and curated information about bicycling in New York City. In my role as web manager at Transportation Alternatives, I've added new functionality, implemented new content sections and troubleshot and fixed bugs.</p>
+</div>
+
+<h4><span class="horizItems"><a target="_blank" href="http://transalt.org">transalt.org</a></span><span class="horizItems">2011 - Present</span></h4>
+<div class="minorText">
+    <p>transalt.org is Transportation Alternatives' online hub for getting New Yorkers involved in the fight to reclaim their city's streets from the automobile. Built on the Drupal platform, it provides opportunities to learn about the organization's mission and ways to get involved. I've created new pages, Drupal content types and views, partially re-themed the site and participated in many other development and maintenance projects.</p>
+</div>
+
+<h4><span class="horizItems"><a target="_blank" href="http://nyccentury.org">nyccentury.org</a>, <a target="_blank" href="http://tourdequeens.org">tourdequeens.org</a> and <a target="_blank" href="http://tourdebrooklyn.org">tourdebrooklyn.org</a></span><span class="horizItems">2011 - Present</span></h4>
+<div class="minorText">
+    <p>This set of Drupal websites provides information about New York City's most popular bike tours. I themed these responsive sites based on PSD mockups and wireframes. The registration system for each site is built in Convoi Luminate Online's Teamraiser product.</p>
+</div>
+
+<h4><span class="horizItems"><a target="_blank" href="http://www.jpmgloballiquidity.com/us/globalcashportal">Global Cash Portal</a></span><span class="horizItems">2005 - 2009</span></h4>
+<div class="minorText">
+    <p>The Global Cash Portal is used by corporate clients of JPMorgan for trading money market funds. Built using Java technologies (JSPs, servlets, Struts MVC framework) and a Sybase database, this platform handled over $4 billion in trades per day. As a web developer, I helped implement major new features, fixed bugs and helped with production support.</p>
+</div>
+
+
+<h3>Personal Work</h3>
+<h4><span class="horizItems"><a target="_blank" href="http://bikenyc.org">Github</a></span><span class="horizItems">2012 - Present</span></h4>
+<div class="minorText">
+    <p>My personal, open source projects including drupal modules, shell scripts and other fun are available on my Github page.</p>
+</div>
+
+<h4><span class="horizItems"><a target="_blank" href="http://patrickcjensen.com">patrickcjensen.com</a></span><span class="horizItems">2012 - Present</span></h4>
+<div class="minorText">
+    <p>This website! As the only developer for this site, it not only provides information about me and my work, but also serves as a playground for me to try out new technologies. It currently uses PHP, AJAX, jQuery, SASS and Compass.</p>
+</div>
+
+<h4><span class="horizItems"><a target="_blank" href="http://littlealbanystreet.com/home.html">Little Albany Street</a></span><span class="horizItems">2002</span></h4>
+<div class="minorText">
+    <p>My first website, just for a laugh. Yes, I did make that logo using Microsoft Paint.</p>
+</div>
--- content/work.php
+++ content/work.php
@@ -2,21 +2,21 @@
 
 <div class='workPosition'>
     <h3>Web Manager</h3>
-    <h4><span class='positionEmployer'>Transportation Alternatives</span><span class='positionLocation'>New York, New York</span></h4>
+    <h4><span class='horizItems'>Transportation Alternatives</span><span class='horizItems'>New York, New York</span></h4>
     <h5>August 2011 - Current</h5>
     <div class='positionDescription'>Implement senior management's vision for the organization's online presence using Drupal and Convio Lumninate Online.</div>
 </div>
 
 <div class='workPosition'>
     <h3>Teaching Assistant: Comprehensive Planning Studio</h3>
-    <h4><span class='positionEmployer'>Rutgers University</span><span class='positionLocation'>New Brunswick, New Jersey</span></h4>
+    <h4><span class='horizItems'>Rutgers University</span><span class='horizItems'>New Brunswick, New Jersey</span></h4>
     <h5>September 2010 - December 2010</h5>
     <div class='positionDescription'>Managed a team of 11 graduate students through a semester-long group project to recommend policies to Ocean City, NJ municipal officials that would spur hotel development in their municipality</div>
 </div>
 
 <div class='workPosition'>
     <h3>Project Management Fellowship</h3>
-    <h4><span class='positionEmployer'>The Louis Berger Group</span><span class='positionLocation'>Mauritius, Africa</span></h4>
+    <h4><span class='horizItems'>The Louis Berger Group</span><span class='horizItems'>Mauritius, Africa</span></h4>
     <h5>June 2010 - August 2010</h5>
     <div class='positionDescription'>Managed an international team of architects, engineers and builders on a project to construct an expansion to Mauritius, Africa's international airport.</div>
 </div>
@@ -24,7 +24,7 @@
 
 <div class='workPosition'>
     <h3>Web Developer</h3>
-    <h4><span class='positionEmployer'>JPMorgan Chase</span><span class='positionLocation'>Columbus, OH</span></h4>
+    <h4><span class='horizItems'>JPMorgan Chase</span><span class='horizItems'>Columbus, OH</span></h4>
     <h5>January 2005 - July 2009</h5>
     <div class='positionDescription'>Developed new features for and fixed defects on a global, money market fund trading web application which handled more than $4 billion in trades per day.</div>
 </div>
