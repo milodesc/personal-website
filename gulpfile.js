6ddb11972c5a4fe66ab275d031d859e153583b17
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-04-04 08:46:51 -0400

    combine gulp tasks into 1 task with a single stream.

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,15 @@ var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
 var prefix = require('gulp-autoprefixer');
 
-// Prefix CSS
-gulp.task('prefix-css', function() {
+// Prefix and Minify CSS
+gulp.task('styles', function() {
     gulp.src('./css/style.css')
         .pipe(prefix("last 2 versions", "> 1%", "ie 8", "ie 7"))
-        .pipe(gulp.dest('./css/'));
-});
-
-// Minify CSS
-gulp.task('minify-css', function() {
-    gulp.src('./css/style.css')
+        .pipe(gulp.dest('./css/'))
         .pipe(minifyCSS())
         .pipe(rename('style.min.css'))
         .pipe(gulp.dest('./css/'));
 });
 
 // Default Task
-gulp.task('default', ['prefix-css', 'minify-css']);
+gulp.task('default', ['styles']);
