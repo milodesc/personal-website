9a5d27a6e9a3bca666b9d8a3d18c986d1fb828a6
Patrick Jensen <patrick.c.jensen@gmail.com>
2014-04-17 07:58:39 -0400

    added analytics

--- index.php
+++ index.php
@@ -44,6 +44,17 @@
 
     <footer>Copyright 2011-<?php echo date (Y); ?></footer>
 
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-24725041-1', 'patrickcjensen.com');
+        ga('send', 'pageview');
+
+    </script>
+
     <!--[if lt IE 9]>
     <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
